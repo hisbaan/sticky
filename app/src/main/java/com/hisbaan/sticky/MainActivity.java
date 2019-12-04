62ba8a9a944364733eab4f6858c35404cfe4ae09
Hisbaan <34548959+hisbaan@users.noreply.github.com>
2019-12-04 11:34:13 -0500

    Dec 4, 2019 | 11:34 AM Changed the animation for the opening of the FAB drawer to a scale animation.

--- app/src/main/java/com/hisbaan/sticky/MainActivity.java
+++ app/src/main/java/com/hisbaan/sticky/MainActivity.java
@@ -1,13 +1,12 @@
 package com.hisbaan.sticky;
 
+//Android imports.
+
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
@@ -16,23 +15,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import java.util.Calendar;
 
+//Java imports.
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean isFABOpen = false;
 
+    private String currentImagePath = null;
+
+    //Member variables.
     private FloatingActionButton mainFab;
     private FloatingActionButton photoFab;
     private FloatingActionButton drawNoteFab;
-    private FloatingActionButton newBoardFab;
-
-    private String currentImagePath = null;
 
     private static final int REQUEST_IMAGE_CAPTURE = 101;
 
@@ -44,7 +44,11 @@ public class MainActivity extends AppCompatActivity {
         mainFab = findViewById(R.id.main_fab);
         photoFab = findViewById(R.id.new_photo_fab);
         drawNoteFab = findViewById(R.id.new_note_fab);
-        newBoardFab = findViewById(R.id.new_board_fab);
+
+        photoFab.setScaleX(0);
+        photoFab.setScaleY(0);
+        drawNoteFab.setScaleX(0);
+        drawNoteFab.setScaleY(0);
 
         mainFab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,16 +64,12 @@ public class MainActivity extends AppCompatActivity {
         photoFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent i = new Intent(getApplicationContext(), CameraActivity.class);
-//                startActivity(i);
-
                 Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
                 if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
-
                     File imageFile = getImageFile();
 
-                    if(imageFile != null) {
+                    if (imageFile != null) {
                         Uri imageUri = FileProvider.getUriForFile(MainActivity.this, "com.hisbaan.android.fileprovider", imageFile);
                         imageTakeIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
 
@@ -82,13 +82,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         drawNoteFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Feature to be added", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        newBoardFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Feature to be added", Toast.LENGTH_SHORT).show();
@@ -119,21 +112,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private File getImageFile() {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
-
-//        Date rightNowDate = Calendar.getInstance().getTime();
-//        char[] rightNowTemp = rightNowDate.toString().toCharArray();
-//        String rightNow = "";
-//
-//        for(int i = 0; i < rightNowTemp.length; i++) {
-//            if (rightNowTemp[i] == ' ') {
-//                rightNowTemp[i] = '-';
-//            }
-//        }
-//
-//        for (char c : rightNowTemp) {
-//            rightNow += c;
-//        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.CANADA).format(new Date());
 
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
@@ -153,12 +132,6 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//
-//            ImageView imageView = findViewById(R.id.image_view);
-//            imageView.setImageBitmap(imageBitmap);
-
             Intent i = new Intent(getApplicationContext(), CameraActivity.class);
             i.putExtra("image_path", currentImagePath);
             startActivity(i);
@@ -169,16 +142,45 @@ public class MainActivity extends AppCompatActivity {
     private void showFABMenu() {
         isFABOpen = true;
         mainFab.animate().rotation(45);
-        photoFab.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
-        drawNoteFab.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
-        newBoardFab.animate().translationY(-getResources().getDimension(R.dimen.standard_155));
+//        photoFab.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+//        drawNoteFab.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+
+//        ScaleAnimation animation1 = new ScaleAnimation(
+//                0f, 1f,
+//                0f, 1f,
+//                Animation.RELATIVE_TO_SELF, 0f,
+//                Animation.RELATIVE_TO_SELF, 0f
+//        );
+//        animation1.setFillAfter(true);
+//        animation1.setDuration(200);
+//
+//        photoFab.startAnimation(animation1);
+//        drawNoteFab.startAnimation(animation1);
+        photoFab.animate().scaleX(1);
+        photoFab.animate().scaleY(1);
+        photoFab.setFocusable(true);
+        photoFab.setClickable(true);
+//
+        drawNoteFab.animate().scaleX(1);
+        drawNoteFab.animate().scaleY(1);
+        drawNoteFab.setFocusable(true);
+        drawNoteFab.setClickable(true);
     }
 
     private void closeFABMenu() {
         isFABOpen = false;
         mainFab.animate().rotation(0);
-        photoFab.animate().translationY(0);
-        drawNoteFab.animate().translationY(0);
-        newBoardFab.animate().translationY(0);
+//        photoFab.animate().translationY(0);
+//        drawNoteFab.animate().translationY(0);
+
+        photoFab.animate().scaleX(0);
+        photoFab.animate().scaleY(0);
+        photoFab.setFocusable(false);
+        photoFab.setClickable(false);
+
+        drawNoteFab.animate().scaleX(0);
+        drawNoteFab.animate().scaleY(0);
+        drawNoteFab.setFocusable(false);
+        drawNoteFab.setClickable(false);
     }
 }
